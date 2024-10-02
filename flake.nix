b8bca417559da55258fccdb771b3190d8577fb5a
Andrew Plaza <github@andrewplaza.dev>
2024-10-02 14:30:40 -0400

    add flake.nix

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+    environments.url = "github:insipx/environments";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { nixpkgs, flake-utils, fenix, environments, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        inherit (pkgs.stdenv) isDarwin;
+        inherit (pkgs.darwin.apple_sdk) frameworks;
+        fenixPkgs = fenix.packages.${system};
+        # probably don't need all these linters
+        linters = import "${environments}/linters.nix" { inherit pkgs; };
+        rust-toolchain = fenixPkgs.fromToolchainFile {
+          file = ./rust-toolchain;
+          sha256 = "sha256-VZZnlyP69+Y3crrLHQyJirqlHrTtGTsyiSnZB8jEvVo=";
+        };
+        nativeBuildInputs = with pkgs; [ pkg-config ];
+        buildInputs = with pkgs;
+          [
+            rust-toolchain
+            rust-analyzer
+            llvmPackages_16.libcxxClang
+            mktemp
+            markdownlint-cli
+            shellcheck
+            buf
+            curl
+            wasm-pack
+            twiggy
+            wasm-bindgen-cli
+            binaryen
+            linters
+            cargo-nextest
+            cargo-udeps
+            cargo-sweep
+            cargo-cache
+            cargo-machete
+          ] ++ lib.optionals isDarwin [
+            libiconv
+            frameworks.CoreServices
+            frameworks.Carbon
+            frameworks.ApplicationServices
+            frameworks.AppKit
+            darwin.cctools
+          ];
+      in
+      with pkgs; {
+        devShells.default = mkShell { inherit buildInputs nativeBuildInputs; };
+      });
+}
