df437741e19c4e1bad27af8d0680479a73472332
Ry Racherbaumer <rygine@users.noreply.github.com>
2024-11-10 16:27:26 -0600

    fix: Update build (#12)
    # Summary
    
    This update adds tree-shaking to the JS build so that unused code paths
    are removed. This removes references to unused files. The terser plugin
    was also added to decrease the size of the source.
    
    - Upgraded node version in `.node-version`
    - Upgraded yarn
    - Removed `package-lock.json`
    - Fixed VSCode `rust-analyzer` settings
    - Updated GitHub workflows
    - Updated README
    - Updated rollup config
    - Added `wasm-pack` and `@rollup/plugin-terser` dependencies
    - Renamed `package.js` to `sqlite3-diesel.js`
    - Added test scripts
    - Added tree-shaking and terser plugin for output
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    ## Release Notes
    
    - **New Features**
    - Updated workflow configurations for better performance and clarity in
    CI/CD processes.
    - Introduced new test scripts for comprehensive browser testing,
    including headless modes.
    
    - **Bug Fixes**
    - Corrected paths and dependencies in the README for improved clarity
    and consistency.
    
    - **Documentation**
    	- Enhanced README with clearer instructions and corrected examples.
    
    - **Chores**
    - Updated Node.js version and Yarn configuration for improved
    compatibility and performance.
    - Streamlined JavaScript code for better readability and
    maintainability.
    
    - **Style**
    - Modernized JavaScript syntax in the async proxy file for enhanced
    clarity.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,28 @@
 import { defineConfig } from "rollup";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import copy from "rollup-plugin-copy";
+import terser from "@rollup/plugin-terser";
 
 export default defineConfig([
   {
-    input: "package.js",
+    input: "sqlite3-diesel.js",
     output: {
-      file: "src/js/wa-sqlite-diesel-bundle.js",
+      file: "src/js/sqlite3-diesel.js",
       format: "es",
     },
+    treeshake: "smallest",
     plugins: [
       nodeResolve(),
+      terser(),
       copy({
         targets: [
           {
-            src:
-              "./node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3.wasm",
+            src: "./node_modules/@sqlite.org/sqlite-wasm/sqlite-wasm/jswasm/sqlite3.wasm",
             dest: "src/js",
           },
         ],
       }),
     ],
-    // external: ["@sqlite.org/sqlite-wasm"],
   },
   {
     input:
@@ -30,5 +31,6 @@ export default defineConfig([
       file: "src/js/sqlite3-opfs-async-proxy.js",
       format: "es",
     },
+    plugins: [terser()],
   },
 ]);
